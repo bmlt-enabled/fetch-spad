55ba9785a79b89ddd6f23d42ecea37d077aa8c36
Patrick Joyce <pjaudiomv@gmail.com>
2025-03-29 10:48:25 -0400

    French language fixes

--- fetch-spad-plugin.php
+++ fetch-spad-plugin.php
@@ -6,7 +6,7 @@
  * Install:           Drop this directory into the "wp-content/plugins/" directory and activate it.
  * Contributors:      pjaudiomv, bmltenabled
  * Author:            bmltenabled
- * Version:           1.2.2
+ * Version:           1.2.3
  * Requires PHP:      8.1
  * Requires at least: 6.2
  * License:           GPL v2 or later
