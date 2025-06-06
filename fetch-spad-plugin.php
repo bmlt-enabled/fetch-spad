ebc7906f6abd3fa262281f8e58bedc4884e8224f
pjaudiomv <pjaudiomv@gmail.com>
2025-06-06 11:33:24 -0400

    update library

--- fetch-spad-plugin.php
+++ fetch-spad-plugin.php
@@ -6,7 +6,7 @@
  * Install:           Drop this directory into the "wp-content/plugins/" directory and activate it.
  * Contributors:      pjaudiomv, bmltenabled
  * Author:            bmltenabled
- * Version:           1.3.1
+ * Version:           1.3.2
  * Requires PHP:      8.1
  * Requires at least: 6.2
  * License:           GPL v2 or later
